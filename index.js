6153699ac5bd19af3ded008d399318b7bd3e603c
veer2238 <veer2238rajput@gmail.com>
2024-05-26 20:02:45 +0530

    index config

--- index.js
+++ index.js
@@ -5,9 +5,50 @@ const app = express();
 app.use('/files', express.static(path.join(__dirname, 'files')));
 
 
-app.get('/html',(req,res) =>{
-    res.send('hid')
-})
+app.get('/v-extechsolution/html-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'html.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/python-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'python.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/css-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'css.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/css-advance-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'css_advance.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/javascript-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'javascript.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/reactjs-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'reactjs.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/sql-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'sql.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/clanguage-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'clanguage.pdf');
+    res.sendFile(filePath);
+  });
+
+  app.get('/v-extechsolution/cplus-pdf-download', (req, res) => {
+    const filePath = path.join(__dirname, 'files', 'c++.pdf');
+    res.sendFile(filePath);
+  });
 
 
 app.listen(3099, () => {
