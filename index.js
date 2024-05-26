0a99f79d69ef578324c34257f2a2d2f32494cee1
veer2238 <veer2238rajput@gmail.com>
2024-05-26 19:45:59 +0530

    index config

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/files', express.static(path.join(__dirname, 'files')));
+
+
+app.get('/html',(req,res) =>{
+    res.send('hi')
+})
+
+
+app.listen(3099, () => {
+  console.log('server connected');
+});
